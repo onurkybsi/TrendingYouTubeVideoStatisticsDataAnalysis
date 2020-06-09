de147054834118f6de6d660e65927e57ae58a315
onurkybsi <onurbpm@outlook.com>
2020-06-10 01:39:10 +0300

    Commit16

--- ImplementingTheAlgorithms/DecisionTree.py
+++ ImplementingTheAlgorithms/DecisionTree.py
@@ -4,6 +4,8 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import KFold
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 processedDatas = pd.read_csv("../processedDataset.csv")[["video_id","trending_date","title","channel_title","category","category_id","publish_time","tags","views","likes",
@@ -15,47 +17,53 @@ processedDatas = pd.read_csv("../processedDataset.csv")[["video_id","trending_da
 ]]
 
 
-#region Verilen view sayısına gore videonun entertainment kategorisine ait olup olmadığını buluyor.
-
-modelUS = DecisionTreeClassifier()
-
 predictionViews = processedDatas[["views"]].to_numpy()
 
 predictionCat = processedDatas[["entertainment_cat"]].to_numpy()
 
 predictionUS = processedDatas[["us_country"]].to_numpy()
 
-X = predictionViews
-y = predictionCat
-kf = KFold(n_splits=10,shuffle=False)
+#region Verilen view sayısına gore videonun entertainment kategorisine ait olup olmadığını buluyor.
 
-bestResult = 0
-bestConfusion = []
+# modelEntertainment = DecisionTreeClassifier()
 
-for train_index, test_index in kf.split(X):
-    X_train, X_test = X[train_index], X[test_index]
-    y_train, y_test = y[train_index], y[test_index]
-    modelUS.fit(X_train, y_train.ravel())
-    prediction = modelUS.predict(X_test)
-    confusionMatrix = confusion_matrix(y_test, prediction)
-    accuracyScore = performanceMetrics.accuracy(confusionMatrix)
-    if(accuracyScore > bestResult):
-        bestResult = accuracyScore
-        bestConfusion = confusionMatrix    
+# X = predictionViews
+# y = predictionCat
+# kf = KFold(n_splits=10,shuffle=False)
+
+# bestResult = 0
+# bestConfusion = []
+
+# for train_index, test_index in kf.split(X):
+#     X_train, X_test = X[train_index], X[test_index]
+#     y_train, y_test = y[train_index], y[test_index]
+#     modelEntertainment.fit(X_train, y_train.ravel())
+#     prediction = modelEntertainment.predict(X_test)
+#     confusionMatrix = confusion_matrix(y_test, prediction)
+#     accuracyScore = performanceMetrics.accuracy(confusionMatrix)
+#     if(accuracyScore > bestResult):
+#         bestResult = accuracyScore
+#         bestConfusion = confusionMatrix    
+
+# ax = plt.axes()
+# sns.heatmap(bestConfusion, annot=True, fmt="d", ax=ax)
+# ax.set_title("Accuracy score for Entertainment category prediction: {}".format(round(bestResult, 3)))
+# plt.show()
 
 
-# Metrics for entertainment category prediction
-accuracyScore = performanceMetrics.accuracy(bestConfusion)
-print("entertainmentViewsPrediction / Accuracy Score: {}".format(round(accuracyScore, 3)))
+# # Metrics for entertainment category prediction
+# accuracyScore = performanceMetrics.accuracy(bestConfusion)
+# print("entertainmentViewsPrediction / Accuracy Score: {}".format(round(accuracyScore, 3)))
 
-precisionScore = performanceMetrics.precision(bestConfusion)
-print("entertainmentViewsPrediction / Precision Score: {}".format(round(precisionScore, 3)))
+# precisionScore = performanceMetrics.precision(bestConfusion)
+# print("entertainmentViewsPrediction / Precision Score: {}".format(round(precisionScore, 3)))
 
-recallScore = performanceMetrics.recall(bestConfusion)
-print("entertainmentViewsPrediction / Recall Score: {}".format(round(recallScore, 3)))
+# recallScore = performanceMetrics.recall(bestConfusion)
+# print("entertainmentViewsPrediction / Recall Score: {}".format(round(recallScore, 3)))
 
-fMeasureScore = performanceMetrics.fmeasure(bestConfusion)
-print("entertainmentViewsPrediction / F-Mesaure Score: {}".format(round(fMeasureScore, 3)))
+# fMeasureScore = performanceMetrics.fmeasure(bestConfusion)
+# print("entertainmentViewsPrediction / F-Mesaure Score: {}".format(round(fMeasureScore, 3)))
+
 
 #endregion
 
@@ -82,6 +90,11 @@ for train_index, test_index in kf.split(X):
         bestResult = accuracyScore
         bestConfusion = confusionMatrix    
 
+
+ax = plt.axes()
+sns.heatmap(bestConfusion, annot=True, fmt="d", ax=ax)
+ax.set_title("Accuracy score for US location prediction: {}".format(round(bestResult, 3)))
+plt.show()
 
 # Metrics for entertainment category prediction
 accuracyScore = performanceMetrics.accuracy(bestConfusion)
